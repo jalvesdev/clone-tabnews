c02e1ce0a50fa3eda627bce1daf4d78136c11e21
José Alves <54028819+jalvesdev@users.noreply.github.com>
2024-01-23 11:46:52 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Nem todos os dias são bons, mas há algo bom em cada dia.</h1>
+    return <h1>Nem todos os dias são bons, mas há algo bom em cada dia. 😀</h1>
 }
 
 export default Home;
